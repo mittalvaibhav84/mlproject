b02adf2a627cfb01b7ed65b3d0ca9f730163849b
Vaibhav Mittal <mittal.vaibhav84@gmail.com>
2023-12-06 23:52:31 +0530

    logging and exception

--- src/exception.py
+++ src/exception.py
@@ -14,13 +14,13 @@ class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_messsage_details(error_message, error_detail=error_detail)
-        logging.info(self.error_message)
+
     def __str__(self):
         return self.error_message
 
 
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as err:
-        raise CustomException(err, sys)
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as err:
+#         raise CustomException(err, sys)
